b241361b94fa19275747cb1f6a0ed94f957e71b2
Casualet <shaoyiwenet@gmail.com>
2017-06-17 00:33:55 +0800

    use minipage

--- images/onions-for-string.py
+++ images/onions-for-string.py
@@ -10,37 +10,46 @@ colors = [cmap(i) for i in np.linspace(0, 1, 3)]
 
 x = range(4)
 y11 = [10,16,32,48]
-y21 = [10,8,16,32]
+
+y21 = [1000,1008,1024,1040]
+
+y31 = [10,8,16,32]
 
 fig = plt.figure()
+ax = fig.add_subplot(111)
 
 
 xxx=[i for i in np.linspace(0,0.45, 4)]
 
-plt.bar(xxx,y11,0.15,color='b')
+ax.bar(xxx,y11,0.15,color='b',label='DET-STR for strlen=10')
 
 
-xxx=[i for i in np.linspace(0.75,1.2, 4)]
+xxx=[i for i in np.linspace(0.75,1.2,4)]
 
-plt.bar(xxx,y21,0.15,color='r')
+ax.bar(xxx,y21,0.15,color='r',label='DET-STR for strlen=1000')
 
-#y2 = [y11, y21, y31]
-label = ['Single Thread', 'SMT2', 'SMT4']
-# fig = plt.figure(dpi=80)
-x = range(4)
+xxx=[i for i in np.linspace(1.5,1.95, 4)]
+ax.bar(xxx,y31,0.15,color='g',label='DET-OPE')
 
-count = 0
 
 #for i,color in enumerate(colors, start=0):
 #    temp = [m + count / 10 for m in x]
     #plt.bar(temp, y2[i], 0.13, color=color, label=label[i])
 #    count = count + 1.3
 
-#plt.xticks([m + 0.18 for m in range(4)], ['4 Copies', '8 Copies', '16 Copies', '32 Copies'])
+
+#plt.xticks([m*0.15 + 0 for m in range(4)], ['Plain', 'DET-JOIN-STR', 'DET-STR','DET-RND'],rotation=45)
+
+#plt.xticks([m*0.15 + 0.75 for m in range(4)], ['Plain', 'DET-JOIN-STR', 'DET-STR','DET-RND'],rotation=45)
+
+#plt.xticks([m*0.15 + 1.5 for m in range(4)], ['Plain', 'OPE-JOIN-STR', 'OPE-STR','OPE-RND'],rotation=45)
 
 
-#plt.ylim(0,2.5)
+
+plt.ylim(0,1500)
 #plt.xlim(-0.1, 3.5)
-plt.legend()
+ax.legend('onions-for-string')
 
 fig.savefig('onions-for-string.eps')
+
+
